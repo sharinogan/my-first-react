5c01e01c25bc21b7e75f5d09fde1f047750bd025
Sharinogan <voragun2543@gmail.com>
2022-09-27 22:19:29 +0700

    css inline style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './App.css';
 import Post from './Post';
 import Comment from './Comment';
 
 const App = () => {
   return (
-    <div>
-      <Post id="2">
-        <Comment userId="1" data="5 stars" />
-        <Comment userId="8" data="4 stars" />
-      </Post>
+    <div className="App">
+      <div style={{color:'white', backgroundColor:'blue',
+          fontSize:30}}>Hello Inline Style CSS</div>
+      <p style={
+        {
+          color:'red',
+          fontSize: 14,
+          textAlign: 'center'
+        }
+      }>This is the another inline style css</p>
     </div>
   );
 }
