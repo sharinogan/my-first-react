033b3223abda25dabe69b8b4c4674f1dfe6200a4
Sharinogan <voragun2543@gmail.com>
2022-09-27 14:55:22 +0700

    create post component in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Post from './Post';
 
 function App() {
   const name = 'Voragun';
   const num1 = 5;
   const num2 = 10;
   const randomNumber = Math.random();
-  return <div className="App"><p>Hello</p></div>
-  
+  const myJSX = (
+    <p>
+      Content from variable
+    </p>
+  );
+  return (
+    <div className="App">
+      <p>ชื่อของผมคือ { name }</p>
+      <p>Score {num1} + {num2} = {num1 + num2}</p>
+      <p>{randomNumber}</p>
+      <p>
+        {
+          randomNumber < 0.5 ?
+          <div>Lost</div> :
+          <div>Win</div>
+        }
+      </p>
+      { myJSX }
+      < Post />
+    </div>
+  );
 }
 
 export default App;
