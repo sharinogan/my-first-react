8d93aaa93de0193f98e070c75fa6f91373bd4027
Sharinogan <voragun2543@gmail.com>
2022-09-27 23:03:17 +0700

    use state in class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () => {
-  const myPic = 'https://placekitten.com/640/360';
-  return (
-    <div className="container">
-      <img className="img-thubnail" src={myPic} alt="kitten" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: "Voragun",
+    password: "thisIsAbook",
+    address: {
+      houseNo: "123",
+      province: "Bangkok"
+    }
+  };
+  userClick = () => {
+    this.setState({
+      user: "Ronaldo",
+      password: "IloveyouMoreToday123"
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-6 mx-auto mt-5">
+            <div className="card ml-5 mr-5">
+              <div className="card-header text-right">
+                <button className="btn btn-success btn-sm" 
+                  onClick={this.userClick}>
+                    Click me</button>
+              </div>
+              <div className="card-body">
+                user: <span className="ml-2">
+                  {this.state.user}</span> <br />
+                password: <span className="ml-2">
+                  {this.state.password}</span> <br />
+                address: <span className="ml-2">
+                  {this.state.address.houseNo}
+                  </span>
+                  <span className="ml-1">{this.
+                  state.address.province}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
