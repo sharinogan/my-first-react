2ce38619fd7f8cf8536a47163a0fce6eaf32735c
Sharinogan <voragun2543@gmail.com>
2022-09-28 12:39:44 +0700

    Example setState version1

--- src/App.js
+++ src/App.js
@@ -2,49 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
+export default class App extends Component {
   state = {
-    user: "Voragun",
-    password: "thisIsAbook",
-    address: {
-      houseNo: "123",
-      province: "Bangkok"
-    }
+    inputValue: 'Origin Value'
   };
-  userClick = () => {
-    this.setState({
-      user: "Ronaldo",
-      password: "IloveyouMoreToday123"
-    });
-  }
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-6 mx-auto mt-5">
-            <div className="card ml-5 mr-5">
-              <div className="card-header text-right">
-                <button className="btn btn-success btn-sm" 
-                  onClick={this.userClick}>
-                    Click me</button>
-              </div>
-              <div className="card-body">
-                user: <span className="ml-2">
-                  {this.state.user}</span> <br />
-                password: <span className="ml-2">
-                  {this.state.password}</span> <br />
-                address: <span className="ml-2">
-                  {this.state.address.houseNo}
-                  </span>
-                  <span className="ml-1">{this.
-                  state.address.province}</span>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div>
+        <button onClick={this.myFunction}>Click</button>
       </div>
-    );
+    )
+  }
+  myFunction = () => {
+    this.setState (
+      { inputValue: 'New Updated Value'},
+      () => {
+        console.log('After setState Complete');
+        console.log(this.state.inputValue);
+      });
+    console.log('Before setState complete');
+    console.log(this.state.inputValue);
   }
 }
-
-export default App;
